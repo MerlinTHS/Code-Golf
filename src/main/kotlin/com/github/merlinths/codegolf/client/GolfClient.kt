4e640b7bf4ac707c7d481159bb7fff16dcb980ff
Merlin Eden (THS) <m.eden@ths-bs.de>
2023-01-05 10:40:34 +0100

    Update getRanking signature of GolfClient interface

--- src/main/kotlin/com/github/merlinths/codegolf/client/GolfClient.kt
+++ src/main/kotlin/com/github/merlinths/codegolf/client/GolfClient.kt
@@ -1,11 +1,23 @@
 package com.github.merlinths.codegolf.client
 
-import com.github.merlinths.codegolf.model.*
+import com.github.merlinths.codegolf.model.Hole
+import com.github.merlinths.codegolf.model.Language
+import com.github.merlinths.codegolf.model.Result
+import com.github.merlinths.codegolf.model.Solution
 import com.github.merlinths.codegolf.model.ranking.Ranking
 import com.github.merlinths.codegolf.model.ranking.RankingPerspective
 import com.github.merlinths.codegolf.model.ranking.RankingType
 
 interface GolfClient {
-    suspend fun postSolution(hole: Hole, solution: Solution): GolfResult
-    suspend fun getRanking(type: RankingType, perspective: RankingPerspective): Ranking
+    suspend fun postSolution(
+            hole: Hole,
+            solution: Solution
+    ): Result
+
+    suspend fun getRanking(
+            hole: Hole,
+            language: Language,
+            type: RankingType,
+            perspective: RankingPerspective
+    ): Ranking
 }
